e1fcd9ca9629ef18daa26676fe2cffde783f4934
Coleman Escue <99232536+Nam3loc@users.noreply.github.com>
2023-01-24 17:43:33 -0500

    learned about colors and learning about customization through the config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+// https://www.youtube.com/watch?v=ft30zcMlFao&t=5s
+// Time of tutorial: 35:50
+// https://tailwindcss.com/docs/screens
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
+	return (
+		<>
+			<h1 className='border-4 border-green-600'>
+				Hello World
+			</h1>
 
-export default App;
+			<h1 className='text-yellow-600'>
+				Whats Up World
+			</h1>
+		</>
+	);
+}
